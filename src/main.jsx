d0db519753ce3805faa89320e5e2bc92192562c7
Goran Jovic <goran1010jovic@gmail.com>
2025-09-09 11:12:44 +0200

    Add CSS reset

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import "./styles/index.css";
+import "./styles/reset.css";
 
 const router = createBrowserRouter(routes);
 
